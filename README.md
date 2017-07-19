803aa956977ae3d0ce15b802371521a05ab6dd3d
Shogun <shogun@cowtech.it>
2017-07-19 10:23:22 +0200

    Initial version.

---
+++ README.md
@@ -0,0 +1,31 @@
+# package-utils
+
+[![Package Version](https://badge.fury.io/js/package-utils.svg)](http://badge.fury.io/js/package-utils)
+[![Dependency Status](https://gemnasium.com/ShogunPanda/package-utils.svg)](https://gemnasium.com/ShogunPanda/package-utils)
+
+Small utility to manage npm package releases.
+
+https://github.com/ShogunPanda/package-utils
+
+## Supported implementations.
+
+package-utils supports and has been tested on [NodeJS](http://nodejs.org) 8.0+.
+
+## API Documentation
+
+The API documentation can be found [here](https://shogunpanda.github.io/package-utils).
+
+## Contributing to package-utils
+
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet.
+* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it.
+* Fork the project.
+* Start a feature/bugfix branch.
+* Commit and push until you are happy with your contribution.
+* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+
+## Copyright
+
+Copyright (C) 2017 and above Shogun <shogun@cowtech.it>.
+
+Licensed under the MIT license, which can be found at http://opensource.org/licenses/MIT.
---
+++ index.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+/*
+ * This file is part of the package-utils package. Copyright (C) 2017 and above Shogun <shogun@cowtech.it>.
+ * Licensed under the MIT license, which can be found at http://www.opensource.org/licenses/mit-license.php.
+ */
+
+const fs = require('fs');
+const cli = require('commander');
+const semver = require('semver');
+const childProcess = require('child_process');
+
+const parseVersion = function(version){
+  if(version.match(/^\d/))
+    return version;
+
+  return semver.inc(require('./package.json').version, version);
+};
+
+const loadChangelog = function(){
+  const raw = fs.readFileSync('./CHANGELOG.md', 'utf8').trim();
+
+  return raw.split('\n').reduce((accu, line) => {
+    line = line.trim();
+
+    if(line.startsWith('###')) // New version
+      accu.unshift([line.substring(4), []]);
+    else if(line.startsWith('* ')) // Entry item
+      accu[0][1].push(line.substring(2));
+
+    return accu;
+  }, []).reverse();
+};
+
+const saveChangelog = function(changelog){
+  // Format file for printing
+  const raw = changelog.map(([version, entries]) => {
+    const rawEntries = entries.map(e => `* ${e}`).join('\n');
+
+    return `### ${version}\n\n${rawEntries}\n`;
+  }).join('\n');
+
+  // Write the file and commit the repository.
+  fs.writeFileSync('./CHANGELOG.md', raw, 'utf8');
+  childProcess.execSync('git commit -a -m "Updated CHANGELOG.md."');
+
+  return raw;
+};
+
+const actionChangelog = function(messages, command){
+  const changelog = loadChangelog();
+
+  if(command.version) // If needed, create a new version
+    changelog.unshift([`${parseVersion(command.version)} / ${new Date().toISOString().substring(0, 10)}`, []]);
+
+  // Add new entries
+  changelog[0][1].unshift(...messages);
+
+  // Save the file
+  saveChangelog(changelog);
+};
+
+const actionRelease = function(version, messages, command){
+  if(messages.length)
+    actionChangelog(messages, {version});
+
+  console.log(`npm version ${version} --scope ${command.restricted ? 'restricted' : 'public'}`);
+};
+
+cli
+  .description('Small utility to manage npm package releases.')
+  .usage('package-utils COMMAND [ARGS...]');
+
+cli.command('changelog [messages...]').alias('c')
+  .option('-v,--version <VERSION>', 'A new version to start. Support increments like in "npm version".', false)
+  .description('Inserts a new entry in the CHANGELOG.md file.')
+  .action(actionChangelog);
+
+cli.command('release <version> [messages...]').alias('r')
+  .option('-r,--restricted', 'If the package must be released as restricted.')
+  .description('Releases a new version.')
+  .action(actionRelease);
+
+cli.parse(process.argv);
